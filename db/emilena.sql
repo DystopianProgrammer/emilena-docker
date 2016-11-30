ef4892b303c64104e4fb7ce3028e761d773c627f
Geoff Perks <geoff.perks@me.com>
2016-11-30 11:09:30 +0000

    updating DDL and properties

--- db/emilena.sql
+++ db/emilena.sql
@@ -1,177 +1,409 @@
-alter table absence drop constraint FKmjma8bj8usk36lgdyliycmy87;
+--
+-- PostgreSQL database dump
+--
 
-alter table availability drop constraint FK8xuimodai9wqc2jrviehti7gb;
+-- Dumped from database version 9.5.4
+-- Dumped by pg_dump version 9.5.4
 
-alter table clients_staff drop constraint FKdhbdgr1njoyjkbldid435riky;
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
 
-alter table clients_staff drop constraint FKqvgj46ri8ja8xlfn3x28phy3i;
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: -
+--
 
-alter table invoice drop constraint FKovxik7m0rxsf4ip8ym0pifd8t;
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
-alter table person drop constraint FKl2f58fmjq95tny1bjm2cp8wsq;
 
-alter table rota_entry drop constraint FKadsq4jno4v8kojdyplrs1oxk3;
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: -
+--
 
-alter table rota_entry drop constraint FK7ofulalwncq5igwbfgn15i1j8;
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 
-alter table rota_item drop constraint FK20mxnfaegj3n7ys354us46ttg;
 
-alter table rota_item drop constraint FKjydd0slyj9roud6nobmfvxjn4;
+SET search_path = public, pg_catalog;
 
-alter table system_user drop constraint FKxxsxrm60pwt1xexvao9bf1r1;
+SET default_tablespace = '';
 
-alter table tbl_roles drop constraint FKq45qsdxvocopwsx1h7qbuwrnm;
+SET default_with_oids = false;
 
-drop table if exists absence cascade;
+--
+-- Name: address; Type: TABLE; Schema: public; Owner: -
+--
 
-drop table if exists availability cascade;
-
-drop table if exists clients_staff cascade;
-
-drop table if exists invoice cascade;
-
-drop table if exists person cascade;
-
-drop table if exists rota cascade;
-
-drop table if exists rota_entry cascade;
-
-drop table if exists rota_item cascade;
-
-drop table if exists system_user cascade;
-
-drop table if exists tbl_roles cascade;
-
-drop table if exists traffic cascade;
-
-drop sequence hibernate_sequence;
-create sequence hibernate_sequence start 1 increment 1;
-
-create table absence (
-    id int8 not null,
-    absenceType int4,
-    date bytea,
-    reason varchar(255),
-    person_id int8,
-    primary key (id)
+CREATE TABLE address (
+    id bigint NOT NULL,
+    first_line character varying(255),
+    post_code character varying(255),
+    second_line character varying(255),
+    town character varying(255),
+    location_id bigint
 );
 
-create table availability (
-    id int8 not null,
-    day_of_week varchar(255),
-    from_time bytea not null,
-    to_time bytea not null,
-    person_id int8,
-    primary key (id)
+
+--
+-- Name: availability; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE availability (
+    id bigint NOT NULL,
+    day_of_week character varying(255),
+    from_time bytea NOT NULL,
+    to_time bytea NOT NULL,
+    person_id bigint
 );
 
-create table clients_staff (
-    client_id int8 not null,
-    staff_id int8 not null
+
+--
+-- Name: clients_staff; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE clients_staff (
+    client_id bigint NOT NULL,
+    staff_id bigint NOT NULL
 );
 
-create table invoice (
-    id int8 not null,
-    amount numeric(19, 2),
+
+--
+-- Name: hibernate_sequence; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE hibernate_sequence
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: invoice; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE invoice (
+    id bigint NOT NULL,
+    amount numeric(19,2),
     created bytea,
-    duration int8,
+    duration bigint,
     issued bytea,
-    rota_item_id int8,
-    primary key (id)
+    rota_item_id bigint
 );
 
-create table person (
-    discriminator varchar(31) not null,
-    id int8 not null,
+
+--
+-- Name: location; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE location (
+    id bigint NOT NULL,
+    latitude double precision,
+    longitude double precision
+);
+
+
+--
+-- Name: person; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE person (
+    discriminator character varying(31) NOT NULL,
+    id bigint NOT NULL,
     active boolean,
-    first_line varchar(255),
-    house_number varchar(255),
-    post_code varchar(255),
-    second_line varchar(255),
-    town varchar(255),
-    date_of_birth timestamp,
-    email varchar(255),
-    forename varchar(255) not null,
-    personType varchar(255) not null,
-    preferences varchar(1000),
-    surname varchar(255) not null,
-    telephone_number varchar(255),
-    contractType varchar(255),
-    contracted_hours int4,
-    staffType varchar(255),
-    system_user_id int8,
-    primary key (id)
+    date_of_birth timestamp without time zone,
+    email character varying(255),
+    forename character varying(255) NOT NULL,
+    persontype character varying(255) NOT NULL,
+    preferences character varying(1000),
+    surname character varying(255) NOT NULL,
+    telephone_number character varying(255),
+    contracttype character varying(255),
+    contracted_hours integer,
+    stafftype character varying(255),
+    person_id bigint,
+    system_user_id bigint
 );
 
-create table rota (
-    id int8 not null,
-    week_commencing bytea,
-    primary key (id)
+
+--
+-- Name: rota; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE rota (
+    id bigint NOT NULL,
+    week_commencing bytea
 );
 
-create table rota_entry (
-    rota_id int8 not null,
-    rota_item_id int8 not null
+
+--
+-- Name: rota_entry; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE rota_entry (
+    rota_id bigint NOT NULL,
+    rota_item_id bigint NOT NULL
 );
 
-create table rota_item (
-    id int8 not null,
-    dayOfWeek varchar(255),
+
+--
+-- Name: rota_item; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE rota_item (
+    id bigint NOT NULL,
+    dayofweek character varying(255),
     finish_time bytea,
     start_time bytea,
     support_date bytea,
-    client_id int8,
-    staff_id int8,
-    primary key (id)
+    client_id bigint,
+    staff_id bigint
 );
 
-create table system_user (
-    id int8 not null,
-    password varchar(255) not null,
-    user_name varchar(255) not null,
-    staff_id int8,
-    primary key (id)
+
+--
+-- Name: system_user; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE system_user (
+    id bigint NOT NULL,
+    password character varying(255) NOT NULL,
+    user_name character varying(255) NOT NULL,
+    staff_id bigint
 );
 
-create table tbl_roles (
-    id int8 not null,
-    roles varchar(255) not null
+
+--
+-- Name: tbl_roles; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE tbl_roles (
+    id bigint NOT NULL,
+    roles character varying(255) NOT NULL
 );
 
-create table traffic (
-    id int8 not null,
-    ip_address varchar(255),
-    primary key (id)
+
+--
+-- Name: traffic; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE traffic (
+    id bigint NOT NULL,
+    ip_address character varying(255)
 );
 
-alter table rota_entry add constraint UK_bps1r273bqncc8kcngk2tcw53 unique (rota_item_id);
 
-alter table system_user add constraint UK_204b9ercidw1baj3s3m9lnr33 unique (user_name);
+--
+-- Name: address_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
 
-alter table absence add constraint FKmjma8bj8usk36lgdyliycmy87 foreign key (person_id) references person;
+ALTER TABLE ONLY address
+    ADD CONSTRAINT address_pkey PRIMARY KEY (id);
 
-alter table availability add constraint FK8xuimodai9wqc2jrviehti7gb foreign key (person_id) references person;
 
-alter table clients_staff add constraint FKdhbdgr1njoyjkbldid435riky foreign key (staff_id) references person;
+--
+-- Name: availability_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
 
-alter table clients_staff add constraint FKqvgj46ri8ja8xlfn3x28phy3i foreign key (client_id) references person;
+ALTER TABLE ONLY availability
+    ADD CONSTRAINT availability_pkey PRIMARY KEY (id);
 
-alter table invoice add constraint FKovxik7m0rxsf4ip8ym0pifd8t foreign key (rota_item_id) references rota_item;
 
-alter table person add constraint FKl2f58fmjq95tny1bjm2cp8wsq foreign key (system_user_id) references system_user;
+--
+-- Name: invoice_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
 
-alter table rota_entry add constraint FKadsq4jno4v8kojdyplrs1oxk3 foreign key (rota_item_id) references rota_item;
+ALTER TABLE ONLY invoice
+    ADD CONSTRAINT invoice_pkey PRIMARY KEY (id);
 
-alter table rota_entry add constraint FK7ofulalwncq5igwbfgn15i1j8 foreign key (rota_id) references rota;
 
-alter table rota_item add constraint FK20mxnfaegj3n7ys354us46ttg foreign key (client_id) references person;
+--
+-- Name: location_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
 
-alter table rota_item add constraint FKjydd0slyj9roud6nobmfvxjn4 foreign key (staff_id) references person;
+ALTER TABLE ONLY location
+    ADD CONSTRAINT location_pkey PRIMARY KEY (id);
 
-alter table system_user add constraint FKxxsxrm60pwt1xexvao9bf1r1 foreign key (staff_id) references person;
 
-alter table tbl_roles add constraint FKq45qsdxvocopwsx1h7qbuwrnm foreign key (id) references system_user;
+--
+-- Name: person_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
 
-insert into system_user (id, password, user_name) values (1, 'superuser', 'superuser');
-insert into tbl_roles (id, roles) values (1, 'STAFF');
-insert into tbl_roles (id, roles) values (1, 'ADMIN');
+ALTER TABLE ONLY person
+    ADD CONSTRAINT person_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: rota_item_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY rota_item
+    ADD CONSTRAINT rota_item_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: rota_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY rota
+    ADD CONSTRAINT rota_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: system_user_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY system_user
+    ADD CONSTRAINT system_user_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: traffic_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY traffic
+    ADD CONSTRAINT traffic_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: uk_204b9ercidw1baj3s3m9lnr33; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY system_user
+    ADD CONSTRAINT uk_204b9ercidw1baj3s3m9lnr33 UNIQUE (user_name);
+
+
+--
+-- Name: uk_bps1r273bqncc8kcngk2tcw53; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY rota_entry
+    ADD CONSTRAINT uk_bps1r273bqncc8kcngk2tcw53 UNIQUE (rota_item_id);
+
+
+--
+-- Name: uk_fwmwi44u55bo4rvwsv0cln012; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY person
+    ADD CONSTRAINT uk_fwmwi44u55bo4rvwsv0cln012 UNIQUE (email);
+
+
+--
+-- Name: ukk25bnafs1ur14e4x4ymhy6mcn; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY person
+    ADD CONSTRAINT ukk25bnafs1ur14e4x4ymhy6mcn UNIQUE (surname, telephone_number);
+
+
+--
+-- Name: fk20mxnfaegj3n7ys354us46ttg; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY rota_item
+    ADD CONSTRAINT fk20mxnfaegj3n7ys354us46ttg FOREIGN KEY (client_id) REFERENCES person(id);
+
+
+--
+-- Name: fk3sbwy3lw9ts4hybjslnp1ikri; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY address
+    ADD CONSTRAINT fk3sbwy3lw9ts4hybjslnp1ikri FOREIGN KEY (location_id) REFERENCES location(id);
+
+
+--
+-- Name: fk7ofulalwncq5igwbfgn15i1j8; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY rota_entry
+    ADD CONSTRAINT fk7ofulalwncq5igwbfgn15i1j8 FOREIGN KEY (rota_id) REFERENCES rota(id);
+
+
+--
+-- Name: fk8xuimodai9wqc2jrviehti7gb; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY availability
+    ADD CONSTRAINT fk8xuimodai9wqc2jrviehti7gb FOREIGN KEY (person_id) REFERENCES person(id);
+
+
+--
+-- Name: fkadsq4jno4v8kojdyplrs1oxk3; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY rota_entry
+    ADD CONSTRAINT fkadsq4jno4v8kojdyplrs1oxk3 FOREIGN KEY (rota_item_id) REFERENCES rota_item(id);
+
+
+--
+-- Name: fkdhbdgr1njoyjkbldid435riky; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY clients_staff
+    ADD CONSTRAINT fkdhbdgr1njoyjkbldid435riky FOREIGN KEY (staff_id) REFERENCES person(id);
+
+
+--
+-- Name: fkjydd0slyj9roud6nobmfvxjn4; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY rota_item
+    ADD CONSTRAINT fkjydd0slyj9roud6nobmfvxjn4 FOREIGN KEY (staff_id) REFERENCES person(id);
+
+
+--
+-- Name: fkl2f58fmjq95tny1bjm2cp8wsq; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY person
+    ADD CONSTRAINT fkl2f58fmjq95tny1bjm2cp8wsq FOREIGN KEY (system_user_id) REFERENCES system_user(id);
+
+
+--
+-- Name: fknltyq4hfdyltdte14tpw2m2a0; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY person
+    ADD CONSTRAINT fknltyq4hfdyltdte14tpw2m2a0 FOREIGN KEY (person_id) REFERENCES address(id);
+
+
+--
+-- Name: fkovxik7m0rxsf4ip8ym0pifd8t; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY invoice
+    ADD CONSTRAINT fkovxik7m0rxsf4ip8ym0pifd8t FOREIGN KEY (rota_item_id) REFERENCES rota_item(id);
+
+
+--
+-- Name: fkq45qsdxvocopwsx1h7qbuwrnm; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY tbl_roles
+    ADD CONSTRAINT fkq45qsdxvocopwsx1h7qbuwrnm FOREIGN KEY (id) REFERENCES system_user(id);
+
+
+--
+-- Name: fkqvgj46ri8ja8xlfn3x28phy3i; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY clients_staff
+    ADD CONSTRAINT fkqvgj46ri8ja8xlfn3x28phy3i FOREIGN KEY (client_id) REFERENCES person(id);
+
+
+--
+-- Name: fkxxsxrm60pwt1xexvao9bf1r1; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY system_user
+    ADD CONSTRAINT fkxxsxrm60pwt1xexvao9bf1r1 FOREIGN KEY (staff_id) REFERENCES person(id);
+
+INSERT INTO system_user (id, password, user_name) VALUES (1, 'superuser', 'superuser');
+INSERT INTO tbl_roles (id, roles) VALUES (1, 'STAFF');
+INSERT INTO tbl_roles (id, roles) VALUES (1, 'ADMIN');
