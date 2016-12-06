36ba41c405f5f2fe1f209f900df46a49ea06f550
Geoff Perks <geoff.perks@me.com>
2016-12-06 01:04:32 +0000

    updates

--- db/utility.sql
+++
@@ -1,3 +0,0 @@
-insert into system_user (id, password, user_name) values (1, 'superuser', 'superuser');
-insert into tbl_roles (id, roles) values (1, 'STAFF');
-insert into tbl_roles (id, roles) values (1, 'ADMIN');
